ea4816ea63822f6b41f67ebfe60059f143dda763
DilawarShafiq <dilawar.gopang@gmail.com>
2024-08-08 13:54:20 +0500

    Add files via upload

---
+++ Basic-calculator.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+# Addition
+sum = a+b
+print(sum)
+# Subtraction
+Subtraction = a-b
+print(Subtraction)
+# Division
+Division = a/b
+print(Division)
+# Multiplication
+multiply = a*b
+print(multiply)
